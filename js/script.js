31da9004de293260901c22c3c253b87f4f0e2c0f
moses <mking78911@gmail.com>
2023-03-19 15:04:00 +0000

    api key, current and 5day forecast url

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// APIkey
+const APIkey = "bf7122aa0b76f70b625211a786786f16";
+
+// weatherToday URL
+const todayURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + APIkey;
+
+// 5 dayforecastURL
+const forecastURL =  "https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=" + APIkey;
